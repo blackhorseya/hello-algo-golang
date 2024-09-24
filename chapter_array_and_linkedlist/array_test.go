f443c9e61b62b98bbab2f93016b893b76ea4caf5
Sean Zheng <blackhorseya@gmail.com>
2024-09-24 10:48:57 +0800

    test: consolidate array handling in test files
    - Add a new test file for arrays in `chapter_array_and_linkedlist`
    - Define and initialize arrays in the `TestArray` function
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ chapter_array_and_linkedlist/array_test.go
@@ -0,0 +1,15 @@
+package chapter_array_and_linkedlist
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestArray(t *testing.T) {
+	// initialize an array
+	var arr [5]int
+	fmt.Println("array arr =", arr)
+
+	nums := []int{1, 3, 2, 5, 4}
+	fmt.Println("array nums =", nums)
+}
