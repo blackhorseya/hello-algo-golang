5380cdc9f95e0b931d5da01f7e7f796da4cb9875
Sean Zheng <blackhorseya@gmail.com>
2024-09-24 11:49:53 +0800

    feat: refactor linked list functions
    - Insert a function to insert a node into a linked list
    - Insert a function to remove an item from a linked list
    - Insert a function to access a node by index in a linked list
    - Insert a function to find a node by target value in a linked list
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- chapter_array_and_linkedlist/linked_list.go
+++ chapter_array_and_linkedlist/linked_list.go
@@ -1,1 +1,45 @@
 package chapter_array_and_linkedlist
+
+import (
+	. "github.com/blackhorseya/hello-algo-golang/pkg"
+)
+
+func insertNode(n0 *ListNode, P *ListNode) {
+	n1 := n0.Next
+	P.Next = n1
+	n0.Next = P
+}
+
+func removeItem(n0 *ListNode) {
+	if n0.Next == nil {
+		return
+	}
+
+	P := n0.Next
+	n1 := P.Next
+	n0.Next = n1
+}
+
+func access(head *ListNode, index int) *ListNode {
+	for i := 0; i < index; i++ {
+		if head == nil {
+			return nil
+		}
+		head.Next = head
+	}
+
+	return head
+}
+
+func findNode(head *ListNode, target int) int {
+	index := 0
+	for head != nil {
+		if head.Val == target {
+			return index
+		}
+		head = head.Next
+		index++
+	}
+
+	return -1
+}
