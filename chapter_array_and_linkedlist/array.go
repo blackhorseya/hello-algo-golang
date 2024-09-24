7682f79f90c4956b8f6739744ff1176da702c4f4
Sean Zheng <blackhorseya@gmail.com>
2024-09-24 11:04:05 +0800

    feat: consolidate number manipulation functions across files
    - Add a function to insert a number at a specified index
    - Add a function to remove a number from a specified index
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- chapter_array_and_linkedlist/array.go
+++ chapter_array_and_linkedlist/array.go
@@ -5,5 +5,27 @@ import (
 )
 
 func randomAccess(nums []int) (randonNum int) {
-	return nums[rand.Intn(len(nums))]
+	// generate a random index
+	randomIndex := rand.Intn(len(nums))
+
+	// get the random number
+	randomNum := nums[randomIndex]
+
+	return randomNum
+}
+
+func insert(nums []int, num int, index int) {
+	// move the numbers after the index
+	for i := len(nums) - 1; i > index; i-- {
+		nums[i] = nums[i-1]
+	}
+
+	// insert the number
+	nums[index] = num
+}
+
+func remove(nums []int, index int) {
+	for i := index; i < len(nums)-1; i++ {
+		nums[i] = nums[i+1]
+	}
 }
