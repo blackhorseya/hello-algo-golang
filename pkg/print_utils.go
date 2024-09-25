32da19253c6f008b214427051183a53f1021440c
Sean Zheng <blackhorseya@gmail.com>
2024-09-25 14:21:32 +0800

    test: implement `PrintTree` function and add tests
    - Add a test file for the binary tree package
    - Implement function `PrintTree` for printing binary trees
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- pkg/print_utils.go
+++ pkg/print_utils.go
@@ -49,3 +49,57 @@ func PrintMap[K comparable, V any](m map[K]V) {
 		fmt.Println(key, "->", value)
 	}
 }
+
+// PrintTree 打印二叉树
+func PrintTree(root *TreeNode) {
+	printTreeHelper(root, nil, false)
+}
+
+// printTreeHelper 打印二叉树
+// This tree printer is borrowed from TECHIE DELIGHT
+// https://www.techiedelight.com/c-program-print-binary-tree/
+func printTreeHelper(root *TreeNode, prev *trunk, isRight bool) {
+	if root == nil {
+		return
+	}
+	prevStr := "    "
+	trunk := newTrunk(prev, prevStr)
+	printTreeHelper(root.Right, trunk, true)
+	if prev == nil {
+		trunk.str = "———"
+	} else if isRight {
+		trunk.str = "/———"
+		prevStr = "   |"
+	} else {
+		trunk.str = "\\———"
+		prev.str = prevStr
+	}
+	showTrunk(trunk)
+	fmt.Println(root.Val)
+	if prev != nil {
+		prev.str = prevStr
+	}
+	trunk.str = "   |"
+	printTreeHelper(root.Left, trunk, false)
+}
+
+type trunk struct {
+	prev *trunk
+	str  string
+}
+
+func newTrunk(prev *trunk, str string) *trunk {
+	return &trunk{
+		prev: prev,
+		str:  str,
+	}
+}
+
+func showTrunk(t *trunk) {
+	if t == nil {
+		return
+	}
+
+	showTrunk(t.prev)
+	fmt.Print(t.str)
+}
