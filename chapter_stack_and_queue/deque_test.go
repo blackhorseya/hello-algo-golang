47a1f78160ed336c3aa7944d2efc3e34a3188ce9
Sean Zheng <blackhorseya@gmail.com>
2024-09-25 12:55:28 +0800

    feat: refactor deque implementation and add linked list deque
    - Update comments in `deque_test.go` from "初始化双端队列" to "初始化双向队列"
    - Add new file `linkedlist_deque.go` implementing a linked list deque
    - Implement methods for pushing, popping, peeking, and checking emptiness in the linked list deque
    - Define functions to access the first and last elements of the deque
    - Add a benchmark function for the linked list deque with push and pop operations
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- chapter_stack_and_queue/deque_test.go
+++ chapter_stack_and_queue/deque_test.go
@@ -9,7 +9,8 @@ import (
 )
 
 func TestDeque(t *testing.T) {
-	/* 初始化双端队列 */
+	/* 初始化双向队列 */
+	// 在 Go 中，将 list 作为双向队列使用
 	deque := list.New()
 
 	/* 元素入队 */
@@ -42,4 +43,95 @@ func TestDeque(t *testing.T) {
 	/* 判断双向队列是否为空 */
 	isEmpty := deque.Len() == 0
 	fmt.Println("双向队列是否为空 =", isEmpty)
+}
+
+// func TestArrayDeque(t *testing.T) {
+// 	/* 初始化双向队列 */
+// 	// 在 Go 中，将 list 作为双向队列使用
+// 	deque := newArrayDeque(16)
+//
+// 	/* 元素入队 */
+// 	deque.pushLast(3)
+// 	deque.pushLast(2)
+// 	deque.pushLast(5)
+// 	fmt.Print("双向队列 deque = ")
+// 	PrintSlice(deque.toSlice())
+//
+// 	/* 访问元素 */
+// 	peekFirst := deque.peekFirst()
+// 	fmt.Println("队首元素 peekFirst =", peekFirst)
+// 	peekLast := deque.peekLast()
+// 	fmt.Println("队尾元素 peekLast =", peekLast)
+//
+// 	/* 元素入队 */
+// 	deque.pushLast(4)
+// 	fmt.Print("元素 4 队尾入队后 deque = ")
+// 	PrintSlice(deque.toSlice())
+// 	deque.pushFirst(1)
+// 	fmt.Print("元素 1 队首入队后 deque = ")
+// 	PrintSlice(deque.toSlice())
+//
+// 	/* 元素出队 */
+// 	popFirst := deque.popFirst()
+// 	fmt.Print("队首出队元素 popFirst = ", popFirst, "，队首出队后 deque = ")
+// 	PrintSlice(deque.toSlice())
+// 	popLast := deque.popLast()
+// 	fmt.Print("队尾出队元素 popLast = ", popLast, "，队尾出队后 deque = ")
+// 	PrintSlice(deque.toSlice())
+//
+// 	/* 获取双向队列的长度 */
+// 	size := deque.size()
+// 	fmt.Println("双向队列长度 size =", size)
+//
+// 	/* 判断双向队列是否为空 */
+// 	isEmpty := deque.isEmpty()
+// 	fmt.Println("双向队列是否为空 =", isEmpty)
+// }
+
+func TestLinkedListDeque(t *testing.T) {
+	// 初始化队列
+	deque := newLinkedListDeque()
+
+	// 元素入队
+	deque.pushLast(2)
+	deque.pushLast(5)
+	deque.pushLast(4)
+	deque.pushFirst(3)
+	deque.pushFirst(1)
+	fmt.Print("队列 deque = ")
+	PrintList(deque.toList())
+
+	// 访问队首元素
+	front := deque.peekFirst()
+	fmt.Println("队首元素 front =", front)
+	rear := deque.peekLast()
+	fmt.Println("队尾元素 rear =", rear)
+
+	// 元素出队
+	popFirst := deque.popFirst()
+	fmt.Print("队首出队元素 popFirst = ", popFirst, "，队首出队后 deque = ")
+	PrintList(deque.toList())
+	popLast := deque.popLast()
+	fmt.Print("队尾出队元素 popLast = ", popLast, "，队尾出队后 deque = ")
+	PrintList(deque.toList())
+
+	// 获取队的长度
+	size := deque.size()
+	fmt.Println("队的长度 size =", size)
+
+	// 判断是否为空
+	isEmpty := deque.isEmpty()
+	fmt.Println("队是否为空 =", isEmpty)
+}
+
+// BenchmarkLinkedListDeque 67.92 ns/op in Mac M1 Pro
+func BenchmarkLinkedListDeque(b *testing.B) {
+	deque := newLinkedListDeque()
+	// use b.N for looping
+	for i := 0; i < b.N; i++ {
+		deque.pushLast(777)
+	}
+	for i := 0; i < b.N; i++ {
+		deque.popFirst()
+	}
 }
