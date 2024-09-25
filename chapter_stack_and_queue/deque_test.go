426775303b2d8d85a52327ad7953fc89df843a9b
Sean Zheng <blackhorseya@gmail.com>
2024-09-25 13:00:48 +0800

    feat: implement array-based double-ended queue with benchmarks
    - Add a new file `array_deque.go` implementing a double-ended queue using a circular array
    - Define struct `arrayDeque` with fields for storing elements, front pointer, queue size, and capacity
    - Implement functions for initializing, getting size, checking if empty, calculating array index, pushing elements to front and back, and popping elements from front and back
    - Implement functions for peeking at the first and last elements, and converting the queue to a slice for printing
    - Modify `deque_test.go` to test the array-based deque instead of a linked list deque
    - Add a benchmark test for `arrayDeque` pushing and popping elements
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- chapter_stack_and_queue/deque_test.go
+++ chapter_stack_and_queue/deque_test.go
@@ -45,48 +45,48 @@ func TestDeque(t *testing.T) {
 	fmt.Println("双向队列是否为空 =", isEmpty)
 }
 
-// func TestArrayDeque(t *testing.T) {
-// 	/* 初始化双向队列 */
-// 	// 在 Go 中，将 list 作为双向队列使用
-// 	deque := newArrayDeque(16)
-//
-// 	/* 元素入队 */
-// 	deque.pushLast(3)
-// 	deque.pushLast(2)
-// 	deque.pushLast(5)
-// 	fmt.Print("双向队列 deque = ")
-// 	PrintSlice(deque.toSlice())
-//
-// 	/* 访问元素 */
-// 	peekFirst := deque.peekFirst()
-// 	fmt.Println("队首元素 peekFirst =", peekFirst)
-// 	peekLast := deque.peekLast()
-// 	fmt.Println("队尾元素 peekLast =", peekLast)
-//
-// 	/* 元素入队 */
-// 	deque.pushLast(4)
-// 	fmt.Print("元素 4 队尾入队后 deque = ")
-// 	PrintSlice(deque.toSlice())
-// 	deque.pushFirst(1)
-// 	fmt.Print("元素 1 队首入队后 deque = ")
-// 	PrintSlice(deque.toSlice())
-//
-// 	/* 元素出队 */
-// 	popFirst := deque.popFirst()
-// 	fmt.Print("队首出队元素 popFirst = ", popFirst, "，队首出队后 deque = ")
-// 	PrintSlice(deque.toSlice())
-// 	popLast := deque.popLast()
-// 	fmt.Print("队尾出队元素 popLast = ", popLast, "，队尾出队后 deque = ")
-// 	PrintSlice(deque.toSlice())
-//
-// 	/* 获取双向队列的长度 */
-// 	size := deque.size()
-// 	fmt.Println("双向队列长度 size =", size)
-//
-// 	/* 判断双向队列是否为空 */
-// 	isEmpty := deque.isEmpty()
-// 	fmt.Println("双向队列是否为空 =", isEmpty)
-// }
+func TestArrayDeque(t *testing.T) {
+	/* 初始化双向队列 */
+	// 在 Go 中，将 list 作为双向队列使用
+	deque := newArrayDeque(16)
+
+	/* 元素入队 */
+	deque.pushLast(3)
+	deque.pushLast(2)
+	deque.pushLast(5)
+	fmt.Print("双向队列 deque = ")
+	PrintSlice(deque.toSlice())
+
+	/* 访问元素 */
+	peekFirst := deque.peekFirst()
+	fmt.Println("队首元素 peekFirst =", peekFirst)
+	peekLast := deque.peekLast()
+	fmt.Println("队尾元素 peekLast =", peekLast)
+
+	/* 元素入队 */
+	deque.pushLast(4)
+	fmt.Print("元素 4 队尾入队后 deque = ")
+	PrintSlice(deque.toSlice())
+	deque.pushFirst(1)
+	fmt.Print("元素 1 队首入队后 deque = ")
+	PrintSlice(deque.toSlice())
+
+	/* 元素出队 */
+	popFirst := deque.popFirst()
+	fmt.Print("队首出队元素 popFirst = ", popFirst, "，队首出队后 deque = ")
+	PrintSlice(deque.toSlice())
+	popLast := deque.popLast()
+	fmt.Print("队尾出队元素 popLast = ", popLast, "，队尾出队后 deque = ")
+	PrintSlice(deque.toSlice())
+
+	/* 获取双向队列的长度 */
+	size := deque.size()
+	fmt.Println("双向队列长度 size =", size)
+
+	/* 判断双向队列是否为空 */
+	isEmpty := deque.isEmpty()
+	fmt.Println("双向队列是否为空 =", isEmpty)
+}
 
 func TestLinkedListDeque(t *testing.T) {
 	// 初始化队列
@@ -124,7 +124,19 @@ func TestLinkedListDeque(t *testing.T) {
 	fmt.Println("队是否为空 =", isEmpty)
 }
 
-// BenchmarkLinkedListDeque 67.92 ns/op in Mac M1 Pro
+// BenchmarkArrayDeque 1.033 ns/op in Mac M1 Pro
+func BenchmarkArrayDeque(b *testing.B) {
+	deque := newArrayDeque(1000)
+	// use b.N for looping
+	for i := 0; i < b.N; i++ {
+		deque.pushLast(777)
+	}
+	for i := 0; i < b.N; i++ {
+		deque.popFirst()
+	}
+}
+
+// BenchmarkLinkedListDeque 51.57 ns/op in Mac M1 Pro
 func BenchmarkLinkedListDeque(b *testing.B) {
 	deque := newLinkedListDeque()
 	// use b.N for looping
