d1f2e2835ba5987d384e4548b89e43043424426f
Sean Zheng <blackhorseya@gmail.com>
2024-09-25 13:29:03 +0800

    feat: implement hash map with chaining for key-value storage
    - Add a test file for hash collision testing
    - Implement a hash map with chaining for key-value pair storage
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ chapter_hashing/hash_collision_test.go
@@ -0,0 +1,32 @@
+package chapter_hashing
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestHashMapChaining(t *testing.T) {
+	/* 初始化哈希表 */
+	hmap := newHashMapChaining()
+
+	/* 添加操作 */
+	// 在哈希表中添加键值对 (key, value)
+	hmap.put(12836, "小哈")
+	hmap.put(15937, "小啰")
+	hmap.put(16750, "小算")
+	hmap.put(13276, "小法")
+	hmap.put(10583, "小鸭")
+	fmt.Println("\n添加完成后，哈希表为\nKey -> Value")
+	hmap.print()
+
+	/* 查询操作 */
+	// 向哈希表中输入键 key ，得到值 value
+	name := hmap.get(15937)
+	fmt.Println("\n输入学号 15937 ，查询到姓名", name)
+
+	/* 删除操作 */
+	// 在哈希表中删除键值对 (key, value)
+	hmap.remove(12836)
+	fmt.Println("\n删除 12836 后，哈希表为\nKey -> Value")
+	hmap.print()
+}
