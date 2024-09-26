3d235936219b6f37858efd2bb6c4b1eec00a3251
Sean Zheng <blackhorseya@gmail.com>
2024-09-26 10:13:47 +0800

    feat: refactor tree traversal functions and tests
    - Add functions for pre-order, in-order, and post-order tree traversal
    - Implement tests for pre-order, in-order, and post-order tree traversal
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ chapter_tree/binary_tree_dfs.go
@@ -0,0 +1,40 @@
+package chapter_tree
+
+import (
+	. "github.com/blackhorseya/hello-algo-golang/pkg"
+)
+
+var nums []any
+
+/* 前序走訪 */
+func preOrder(node *TreeNode) {
+	if node == nil {
+		return
+	}
+	// 訪問優先順序：根節點 -> 左子樹 -> 右子樹
+	nums = append(nums, node.Val)
+	preOrder(node.Left)
+	preOrder(node.Right)
+}
+
+/* 中序走訪 */
+func inOrder(node *TreeNode) {
+	if node == nil {
+		return
+	}
+	// 訪問優先順序：左子樹 -> 根節點 -> 右子樹
+	inOrder(node.Left)
+	nums = append(nums, node.Val)
+	inOrder(node.Right)
+}
+
+/* 後序走訪 */
+func postOrder(node *TreeNode) {
+	if node == nil {
+		return
+	}
+	// 訪問優先順序：左子樹 -> 右子樹 -> 根節點
+	postOrder(node.Left)
+	postOrder(node.Right)
+	nums = append(nums, node.Val)
+}
