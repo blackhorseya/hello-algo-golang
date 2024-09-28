5aef1f6fe6003f1cca676d4a3c38dcdebf7500ec
Sean Zheng <blackhorseya@gmail.com>
2024-09-28 16:09:24 +0800

    feat: refactor backtracking algorithm and add tests
    - Add a new file `chapter_backtracking/preorder_traversal_iii_template.go`
    - Implement functions for backtracking algorithm
    - Add tests for the backtracking algorithm in `chapter_backtracking/preorder_traversal_test.go`
    - Initialize a binary tree for testing purposes
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- chapter_backtracking/preorder_traversal_test.go
+++ chapter_backtracking/preorder_traversal_test.go
@@ -63,3 +63,25 @@ func TestPreorderTraversalIIICompact(t *testing.T) {
 		fmt.Println()
 	}
 }
+
+func TestPreorderTraversalIIITemplate(t *testing.T) {
+	/* 初始化二叉树 */
+	root := SliceToTree([]any{1, 7, 3, 4, 5, 6, 7})
+	fmt.Println("\n初始化二叉树")
+	PrintTree(root)
+
+	// 回溯算法
+	res := make([][]*TreeNode, 0)
+	state := make([]*TreeNode, 0)
+	choices := make([]*TreeNode, 0)
+	choices = append(choices, root)
+	backtrackIII(&state, &choices, &res)
+
+	fmt.Println("\n输出所有根节点到节点 7 的路径，路径中不包含值为 3 的节点")
+	for _, path := range res {
+		for _, node := range path {
+			fmt.Printf("%v ", node.Val)
+		}
+		fmt.Println()
+	}
+}
