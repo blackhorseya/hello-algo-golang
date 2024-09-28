87b60be0d0ff41974aff124eecff45c9b925835e
Sean Zheng <blackhorseya@gmail.com>
2024-09-28 16:46:57 +0800

    feat: refactor subset sum calculation and testing logic
    - Add a new file `subset_sum_i.go` to the `chapter_backtracking` directory
    - Implement the backtrackSubsetSumI function for subset sum calculation
    - Implement the subsetSumI function for subset sum calculation
    - Add a new test file `subset_sum_test.go` for testing subset sum functions
    - Implement TestSubsetSumINaive and TestSubsetSumI test functions
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- chapter_backtracking/subset_sum_test.go
+++ chapter_backtracking/subset_sum_test.go
@@ -22,3 +22,17 @@ func TestSubsetSumINaive(t *testing.T) {
 	}
 	fmt.Println("请注意，该方法输出的结果包含重复集合")
 }
+
+func TestSubsetSumI(t *testing.T) {
+	nums := []int{3, 4, 5}
+	target := 9
+	res := subsetSumI(nums, target)
+
+	fmt.Printf("target = " + strconv.Itoa(target) + ", 输入数组 nums = ")
+	PrintSlice(nums)
+
+	fmt.Println("所有和等于 " + strconv.Itoa(target) + " 的子集 res = ")
+	for i := range res {
+		PrintSlice(res[i])
+	}
+}
