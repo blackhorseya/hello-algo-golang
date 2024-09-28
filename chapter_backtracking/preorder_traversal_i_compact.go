e5dbcb710eb1096aa85ee4c4cd3e52b982e0e436
Sean Zheng <blackhorseya@gmail.com>
2024-09-28 15:49:30 +0800

    docs: refactor traversal functions and testing configurations
    - Add a comment to explain recording the solution when the node value is 7 in `preOrderI` function
    - Update the test file to use the correct function `preOrderI` instead of `preOrderTraversalICompact`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- chapter_backtracking/preorder_traversal_i_compact.go
+++ chapter_backtracking/preorder_traversal_i_compact.go
@@ -1,1 +1,18 @@
 package chapter_backtracking
+
+import (
+	. "github.com/blackhorseya/hello-algo-golang/pkg"
+)
+
+/* 前序走訪：例題一 */
+func preOrderI(root *TreeNode, res *[]*TreeNode) {
+	if root == nil {
+		return
+	}
+	if (root.Val).(int) == 7 {
+		// 記錄解
+		*res = append(*res, root)
+	}
+	preOrderI(root.Left, res)
+	preOrderI(root.Right, res)
+}
--- chapter_backtracking/preorder_traversal_test.go
+++ chapter_backtracking/preorder_traversal_test.go
@@ -1,1 +1,25 @@
 package chapter_backtracking
+
+import (
+	"fmt"
+	"testing"
+
+	. "github.com/blackhorseya/hello-algo-golang/pkg"
+)
+
+func TestPreorderTraversalICompact(t *testing.T) {
+	/* 初始化二叉树 */
+	root := SliceToTree([]any{1, 7, 3, 4, 5, 6, 7})
+	fmt.Println("\n初始化二叉树")
+	PrintTree(root)
+
+	// 前序遍历
+	res := make([]*TreeNode, 0)
+	preOrderI(root, &res)
+
+	fmt.Println("\n输出所有值为 7 的节点")
+	for _, node := range res {
+		fmt.Printf("%v ", node.Val)
+	}
+	fmt.Println()
+}
