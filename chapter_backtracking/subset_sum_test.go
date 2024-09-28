8a2bbb696255e778d818c00734995b0bf5c08461
Sean Zheng <blackhorseya@gmail.com>
2024-09-28 16:43:16 +0800

    feat: improve subset sum algorithm implementation
    - Add a new file `subset_sum_i_naive.go` with backtracking logic for subset sums
    - Implement a test for `subsetSumINaive` function in the `subset_sum_test.go` file
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ chapter_backtracking/subset_sum_test.go
@@ -0,0 +1,24 @@
+package chapter_backtracking
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+
+	. "github.com/blackhorseya/hello-algo-golang/pkg"
+)
+
+func TestSubsetSumINaive(t *testing.T) {
+	nums := []int{3, 4, 5}
+	target := 9
+	res := subsetSumINaive(nums, target)
+
+	fmt.Printf("target = " + strconv.Itoa(target) + ", 输入数组 nums = ")
+	PrintSlice(nums)
+
+	fmt.Println("所有和等于 " + strconv.Itoa(target) + " 的子集 res = ")
+	for i := range res {
+		PrintSlice(res[i])
+	}
+	fmt.Println("请注意，该方法输出的结果包含重复集合")
+}
