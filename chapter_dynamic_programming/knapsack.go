d19e283ba84f4e5108803ca6c5b03ad2a53892d9
Sean Zheng <blackhorseya@gmail.com>
2024-09-29 10:16:09 +0800

    feat: refactor knapsack problem solution
    - Add a new file `knapsack.go` for the dynamic programming chapter
    - Implement the `knapsackDFS` function for 0-1 knapsack problem
    - Add a new file `knapsack_test.go` for testing the `knapsackDFS` function
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ chapter_dynamic_programming/knapsack.go
@@ -0,0 +1,20 @@
+package chapter_dynamic_programming
+
+/* 0-1 背包：暴力搜尋 */
+func knapsackDFS(wgt, val []int, i, c int) int {
+	// 若已選完所有物品或背包無剩餘容量，則返回價值 0
+	if i == 0 || c == 0 {
+		return 0
+	}
+
+	// 若超過背包容量，則只能選擇不放入背包
+	if wgt[i-1] > c {
+		return knapsackDFS(wgt, val, i-1, c)
+	}
+
+	// 計算不放入和放入物品 i 的最大價值
+	no := knapsackDFS(wgt, val, i-1, c)
+	yes := knapsackDFS(wgt, val, i-1, c-wgt[i-1]) + val[i-1]
+
+	return max(no, yes)
+}
