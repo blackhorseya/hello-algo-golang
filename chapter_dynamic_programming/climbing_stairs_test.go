73b4407986b198ae8e256a422847e0bdf9282390
Sean Zheng <blackhorseya@gmail.com>
2024-09-29 09:51:58 +0800

    test: refactor cost calculation functions
    - Add tests for `minCostClimbingStairsDPComp` function
    - Implement `minCostClimbingStairsDPComp` function in a new file
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- chapter_dynamic_programming/climbing_stairs_test.go
+++ chapter_dynamic_programming/climbing_stairs_test.go
@@ -34,3 +34,20 @@ func TestClimbingStairsDPComp(t *testing.T) {
 	res := climbingStairsDPComp(n)
 	fmt.Printf("爬 %d 阶楼梯共有 %d 种方案\n", n, res)
 }
+
+func TestMinCostClimbingStairsDPComp(t *testing.T) {
+	cost := []int{0, 1, 10, 1, 1, 1, 10, 1, 1, 10, 1}
+	fmt.Printf("输入楼梯的代价列表为 %v\n", cost)
+
+	res := minCostClimbingStairsDP(cost)
+	fmt.Printf("爬完楼梯的最低代价为 %d\n", res)
+
+	res = minCostClimbingStairsDPComp(cost)
+	fmt.Printf("爬完楼梯的最低代价为 %d\n", res)
+}
+
+// func TestClimbingStairsConstraintDP(t *testing.T) {
+// 	n := 9
+// 	res := climbingStairsConstraintDP(n)
+// 	fmt.Printf("爬 %d 阶楼梯共有 %d 种方案\n", n, res)
+// }
