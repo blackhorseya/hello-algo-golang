f3741d631cd946a6da7d7d25b161be77e562e151
Sean Zheng <blackhorseya@gmail.com>
2024-09-29 10:40:37 +0800

    feat: refactor dynamic programming for coin change variations
    - Add a new file for `coin_change_ii.go` in the `chapter_dynamic_programming` directory
    - Implement the `coinChangeIIDP` function for dynamic programming
    - Implement the `coinChangeIIDPComp` function with space optimization for dynamic programming
    - Modify the `coin_change_test.go` file in the `chapter_dynamic_programming` directory
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- chapter_dynamic_programming/coin_change_test.go
+++ chapter_dynamic_programming/coin_change_test.go
@@ -17,3 +17,16 @@ func TestCoinChange(t *testing.T) {
 	res = coinChangeDPComp(coins, amt)
 	fmt.Printf("凑到目标金额所需的最少硬币数量为 %d\n", res)
 }
+
+func TestCoinChangeII(t *testing.T) {
+	coins := []int{1, 2, 5}
+	amt := 5
+
+	// 动态规划
+	res := coinChangeIIDP(coins, amt)
+	fmt.Printf("凑到目标金额的硬币组合数为 %d\n", res)
+
+	// 空间优化后的动态规划
+	res = coinChangeIIDPComp(coins, amt)
+	fmt.Printf("凑到目标金额的硬币组合数为 %d\n", res)
+}
